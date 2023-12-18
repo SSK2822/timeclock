301999471f5173672b6d139aead7bfb7dd5d085d
Shreyash Kawle <77000727+SSK2822@users.noreply.github.com>
2023-12-18 00:15:08 -0500

    Update ModifiedSketch.js

--- ModifiedSketch.js
+++ ModifiedSketch.js
@@ -1,35 +1,34 @@
 
-// setup() is called once at page-load
+
 function setup() {
-    createCanvas(800, 600); // make an HTML canvas element width x height pixels
+    createCanvas(800, 600); 
     noStroke();
 }
 
-// draw() is called 60 times per second
+
 function draw() {
     let hr = hour();
     let min = minute();
     let sec = second();
 
-    // Clear the background to represent a garden scene
     background(200, 255, 200);
 
     // Draw hours as trees
     for (let i = 0; i < hr; i++) {
         let treeHeight = map(i, 0, 23, 50, 300);
-        fill(100, 50, 0); // Brown color for tree trunk
+        fill(100, 50, 0); 
         rect(33 + i * 33, height - treeHeight, 20, treeHeight); // Adjusted position for 24 trees
-        fill(0, 200, 0); // Green color for tree foliage
+        fill(0, 200, 0); 
         ellipse(43 + i * 33, height - treeHeight, 60, 60);
     }
 
-    // Draw minutes as flowers in a zig-zag pattern at the top
-    let yOffset = 0; // Offset for creating the zig-zag pattern
+    // Draw minutes as clouds
+    let yOffset = 0; 
     for (let i = 0; i < min; i++) {
         let x = 50 + i % 60 * 12;
         let y = 20 + yOffset;
-        yOffset = (yOffset === 0) ? 10 : 0; // Change offset for zig-zag
-        fill(255, 150, 0); // Orange color for flowers
+        yOffset = (yOffset === 0) ? 10 : 0; 
+        fill(255, 150, 0); 
         ellipse(x, y, 10, 10);
     }
 
@@ -40,7 +39,7 @@ function draw() {
         let angle = map(i, 0, 59, 0, TWO_PI) - HALF_PI; // Adjusting angle for starting point
         let x = centerX + cos(angle) * 50;
         let y = centerY + sin(angle) * 50;
-        fill(255, 0, 0); // Red color for second dots
+        fill(255, 0, 0); 
         ellipse(x, y, 5, 5);
     }
 
